9fa624531b09391d19205606d7288ab2710d4098
Sirapob <sintchia@hotmail.com>
2018-08-22 22:41:55 +0700

    Revert "Revert "new project""
    This reverts commit 6f74855f5039ddba4b22d8b1408d1e5512d315d6.

---
+++ MedTest/MedTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MedTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
